d522756140cd196afd25fff6062135dda751e836
Md Jahid Hasan <63962029+jhshakil@users.noreply.github.com>
2022-09-04 13:38:38 +0600

    practice

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
         <div>
             <div>
                 <img src="../assets/logo/stoica-ionela-CoNsEK5iHug-unsplash.jpg" alt="" />
-                <h2>Tutor Academia</h2>
+                <h2>Tutor Academia pull</h2>
             </div>
         </div>
     </div>
